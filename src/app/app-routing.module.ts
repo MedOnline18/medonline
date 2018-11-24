dee7371ffd25c981b0285a618fc1b22f442031f2
Damian Kaminski <damian.kaminski.gdynia@o2.pl>
2018-11-24 20:45:09 +0100

    routing fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
+import { RegisterComponent } from './patients/register/register.component';
 import { PatientsListComponent } from './patients/patients-list/patients-list.component';
 
 const routes: Routes = [
