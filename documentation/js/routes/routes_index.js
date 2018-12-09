f28da674c3014ec4ec1d030a99a101452a657cbc
Damian Kaminski <damian.kaminski.gdynia@o2.pl>
2018-12-09 15:29:28 +0100

    compodoc

--- documentation/js/routes/routes_index.js
+++ documentation/js/routes/routes_index.js
@@ -1,1 +1,1 @@
-var ROUTES_INDEX = {"name":"<root>","kind":"module","className":"AppModule","children":[{"name":"routes","filename":"src/app/app-routing.module.ts","module":"AppRoutingModule","children":[{"path":"","component":"DashboardComponent"},{"path":"login","component":"LoginComponent"},{"path":"register","component":"RegisterComponent"},{"path":"patients","component":"PatientsListComponent"}],"kind":"module"}]}
+var ROUTES_INDEX = {"name":"<root>","kind":"module","className":"AppModule","children":[]}
